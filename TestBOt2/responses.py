20e66515aa349103612910f472e17cbe9b3a7703
Toji <kammcooper@gmail.com>
2024-03-18 12:30:05 -0400

    random stuff

--- TestBOt2/responses.py
+++ TestBOt2/responses.py
@@ -2,14 +2,14 @@ from random import randint
 
 import discord
 
-embed = discord.Embed(
-    colour=discord.Colour.darker_gray(),
-    description="Shotgun warrior",
-    title="Luna Wuna Loadout"
+#embed = discord.Embed(
+  #  colour=discord.Colour.darker_gray(),
+   # description="Shotgun warrior",
+ #   title="Luna Wuna Loadout"
 
-    embed.set_footer(text="Shoresy"),
-    embed.set_author(name="Shoresy")
-    embed.set_thumbnail
+  #  embed.set_footer(text="Shoresy"),
+   # embed.set_author(name="Shoresy")
+   # embed.set_thumbnail
 
 
 
@@ -28,4 +28,5 @@ def get_response(user_input: str) -> str:
         return 'Conditional Finality, Igneous Hammer, The Other Half with way too much mobility'
     elif '!sinuai_skill' in lowered:
         return '-500 elo wood 5'
-    elif ''
+    elif 'Shut up @<1216935709982851113>':
+        return 'Oh, youve just crossed into dangerous territory, my friend. If you dont give me back my snacks, I might have to unleash the fury of a thousand dad jokes upon you. And trust me, you dont want to face that kind of punishment.'
